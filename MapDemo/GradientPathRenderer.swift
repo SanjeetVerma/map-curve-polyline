a45acb8a99810a986e86e7bcd67343e76c95c740
Sanjeet Kumar <sanjeet.kumar@yulu.bike>
2020-06-15 19:21:02 +0530

    fixed curve line

--- MapDemo/GradientPathRenderer.swift
+++ MapDemo/GradientPathRenderer.swift
@@ -1,40 +1,33 @@
  //  Created by Joel Trew on 24/04/2016.
- 
  import MapKit
- 
  /// Draws a given polyline with a gradient fill, use in place of a MKOverlayPathRenderer
  class GradientPathRenderer: MKOverlayPathRenderer {
-    
     /// The polyline to render
     var polyline: MKPolyline
     /// The colors used to draw the gradient
     var colors: [UIColor]
-    
     /// If a border should be rendered to make the line more visible
     var showsBorder: Bool = false
     /// The color of tne border, if showsBorder is true
     var borderColor: UIColor?
-    
     /// Convenience to get an array of CGcolours from UIColors
     private var cgColors: [CGColor] {
         return colors.map({ (color) -> CGColor in
             return color.cgColor
         })
     }
-    
     //MARK: Initializers
     /// Initializes a new Gradient Path Renderer from a given polyline and an array of colors
     ///
     /// - Parameters:
     ///   - polyline: The polyline to render
     ///   - colors: The colours the gardient should contain
-    init(polyline: MKPolyline, colors: [UIColor]) {
+    init(polyline: MKPolyline, colors: [UIColor], showsBorder: Bool) {
         self.polyline = polyline
         self.colors = colors
-        
+        self.showsBorder = showsBorder
         super.init(overlay: polyline)
     }
-    
     /// Initializes a new Gradient Path Renderer from a given polyline and an array of colors, with a border with a defined colour
     ///
     /// - Parameters:
@@ -47,18 +40,14 @@
         self.colors = colors
         self.showsBorder = showsBorder
         self.borderColor = borderColor
-        
         super.init(overlay: polyline)
     }
-    
     //MARK: Override methods
     override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
-        
         /*
          Set path width relative to map zoom scale
          */
         let baseWidth: CGFloat = self.lineWidth / zoomScale
-        
         if self.showsBorder {
             context.setLineWidth(baseWidth * 2)
             context.setLineJoin(CGLineJoin.round)
@@ -67,7 +56,6 @@
             context.setStrokeColor(self.borderColor?.cgColor ?? UIColor.white.cgColor)
             context.strokePath()
         }
-        
         /*
          Create a gradient from the colors provided with evenly spaced stops
          */
@@ -75,44 +63,34 @@
         let stopValues = calculateNumberOfStops()
         let locations: [CGFloat] = stopValues
         let gradient = CGGradient(colorsSpace: colorspace, colors: cgColors as CFArray, locations: locations)
-        
         /*
          Define path properties and add it to context
          */
         context.setLineWidth(baseWidth)
         context.setLineJoin(CGLineJoin.round)
         context.setLineCap(CGLineCap.round)
-        
         context.addPath(self.path)
-        
         /*
          Replace path with stroked version so we can clip
          */
         context.saveGState();
-        
         context.replacePathWithStrokedPath()
         context.clip();
-        
         /*
          Create bounding box around path and get top and bottom points
          */
         let boundingBox = self.path.boundingBoxOfPath
         let gradientStart = boundingBox.origin
         let gradientEnd   = CGPoint(x:boundingBox.maxX, y:boundingBox.maxY)
-        
         /*
          Draw the gradient in the clipped context of the path
          */
         if let gradient = gradient {
             context.drawLinearGradient(gradient, start: gradientStart, end: gradientEnd, options: CGGradientDrawingOptions.drawsBeforeStartLocation);
         }
-        
-        
         context.restoreGState()
-        
         super.draw(mapRect, zoomScale: zoomScale, in: context)
     }
-    
     /*
      Create path from polyline
      Thanks to Adrian Schoenig
@@ -121,9 +99,7 @@
     /*override func createPath() {
         let path: CGMutablePath  = CGMutablePath()
         var pathIsEmpty: Bool = true
-        
         for i in 0...self.polyline.pointCount-1 {
-            
             let point: CGPoint = self.point(for: self.polyline.points()[i])
             if pathIsEmpty {
                 path.move(to: point)
@@ -134,7 +110,6 @@
         }
         self.path = path
     }*/
-    
     override func createPath() {
      // Getting the coordinates from the polyline
      let points = polyline.points()
@@ -144,11 +119,9 @@
      let startPoint = point(for: points[0])
      let endPoint = point(for: points[1])
      let centerPoint = point(for: centerMapPoint)
-
      // I would like to thank a co-worker of mine for the controlPoint formula :)
      let controlPoint = CGPoint(x: centerPoint.x + (startPoint.y - endPoint.y) / 3,
                                 y: centerPoint.y + (endPoint.x - startPoint.x) / 3)
-
      // Defining our new curved path using Bezier path
      let myPath = UIBezierPath()
      myPath.move(to: startPoint)
@@ -157,12 +130,9 @@
      // Mutates the solid line with our curved one
      path = myPath.cgPath
     }
-    
     //MARK: Helper Methods
     private func calculateNumberOfStops() -> [CGFloat] {
-        
         let stopDifference = (1 / Double(cgColors.count))
-        
         return Array(stride(from: 0, to: 1+stopDifference, by: stopDifference))
             .map { (value) -> CGFloat in
                 return CGFloat(value)
--- MapDemo/MapViewController.swift
+++ MapDemo/MapViewController.swift
@@ -11,21 +11,18 @@ import MapKit
 import CoreLocation
 
 class MapViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
-
-
+    
+    
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
-    
     var defaultDelta = 0.01
     var straighLineFlag = false
     var curveLineFlag = false
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationServices()
     }
-    
     func checkLocationServices() {
         self.mapView.showsUserLocation = true
         self.mapView.delegate = self
@@ -37,15 +34,12 @@ class MapViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDel
     func startUpdating() {
         locationManager.startUpdatingLocation()
     }
-    
     func endUpdating() {
         locationManager.stopUpdatingLocation()
     }
-    
     func requestAuthorization() {
         locationManager.requestAlwaysAuthorization()
     }
-    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         switch status {
             case .notDetermined:
@@ -62,78 +56,64 @@ class MapViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDel
                 break
         }
     }
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         self.animateToLocation(location: locations.last!.coordinate)
-//        let LAX = CLLocation(latitude: (locations.last?.coordinate.latitude)!, longitude:(locations.last?.coordinate.longitude)!)
-//        let JFK = CLLocation(latitude: 23.0802, longitude:72.5244)
-        
+        //        let LAX = CLLocation(latitude: (locations.last?.coordinate.latitude)!, longitude:(locations.last?.coordinate.longitude)!)
+        //        let JFK = CLLocation(latitude: 23.0802, longitude:72.5244)addOverlay
         /*let LAX = CLLocation(latitude: 33.9424955, longitude: -118.4080684)
-        let JFK = CLLocation(latitude: 40.6397511, longitude: -73.7789256)*/
-
+         let JFK = CLLocation(latitude: 40.6397511, longitude: -73.7789256)*/
         /*var coordinates = [LAX.coordinate, JFK.coordinate]
-        let geodesicPolyline = MKGeodesicPolyline(coordinates: &coordinates, count: 2)
-        mapView.addOverlay(geodesicPolyline)*/
-        
+         let geodesicPolyline = MKGeodesicPolyline(coordinates: &coordinates, count: 2)
+         mapView.addOverlay(geodesicPolyline)*/
         /*let coordinates = [LAX.coordinate, JFK.coordinate]
-        let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
-        mapView.addOverlay(polyline)*/
-        
+         let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+         mapView.(polyline)*/
         self.createStraightline(locations)
-        self.perform(#selector(createCurveLine(_:)), with: locations, afterDelay: 0.5)
-        
+        self.perform(#selector(createCurveLine(_:)), with: locations, afterDelay: 1.0)
         self.endUpdating()
     }
-    
     func createStraightline( _ locations:[CLLocation]){
         self.straighLineFlag = true
-        let LAX = CLLocation(latitude: (locations.last?.coordinate.latitude)!, longitude:(locations.last?.coordinate.longitude)!)
-        let JFK = CLLocation(latitude: 23.0802, longitude:72.5244)
-        let coordinates = [LAX.coordinate, JFK.coordinate]
+        let source = CLLocation(latitude: (locations.last?.coordinate.latitude)!, longitude:(locations.last?.coordinate.longitude)!)
+        let destination = CLLocation(latitude: 23.0802, longitude:72.5244)
+        let coordinates = [source.coordinate, destination.coordinate]
         let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
-        
+        polyline.title = "straight"
         mapView.addOverlay(polyline)
     }
-    
     @objc func createCurveLine(_ locations:[CLLocation]){
         self.curveLineFlag = true
-        let LAX = CLLocation(latitude: (locations.last?.coordinate.latitude)!, longitude:(locations.last?.coordinate.longitude)!)
-        let JFK = CLLocation(latitude: 23.0802, longitude:72.5244)
-        let coordinates = [LAX.coordinate, JFK.coordinate]
+        let source = CLLocation(latitude: (locations.last?.coordinate.latitude)!, longitude:(locations.last?.coordinate.longitude)!)
+        let destination = CLLocation(latitude: 23.0802, longitude:72.5244)
+        let coordinates = [source.coordinate, destination.coordinate]
         let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
-        
+        polyline.title = "curve"
         mapView.addOverlay(polyline)
     }
-    
-    
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        
-        var renderer = MKOverlayRenderer()
+        var mkPolylineRenderer = MKPolylineRenderer()
+        var mkOverlayPathRenderer = MKOverlayPathRenderer()
         guard let polyline = overlay as? MKPolyline else {
             return MKOverlayRenderer()
         }
-        
-        if self.straighLineFlag{
-            renderer = MKOverlayRenderer(overlay: polyline)
-            renderer.strokeColor = UIColor.lightGray
-            renderer.lineWidth = 5
+        if polyline.title == "straight" {
+            mkPolylineRenderer = MKPolylineRenderer(overlay: polyline)
+            mkPolylineRenderer.strokeColor = UIColor.lightGray
+            mkPolylineRenderer.lineWidth = 2.0
+            return mkPolylineRenderer
         }
-        
-        if self.curveLineFlag{
-            renderer = GradientPathRenderer(polyline: polyline, colors: [UIColor.black])
-            renderer.showsBorder = true
-            renderer.lineWidth = 5.0
-            renderer.createPath()
+        if polyline.title == "curve" {
+            mkOverlayPathRenderer = GradientPathRenderer(polyline: polyline, colors: [UIColor.black], showsBorder: true)
+            mkOverlayPathRenderer.lineWidth = 5.0
+            mkOverlayPathRenderer.createPath()
+            return mkOverlayPathRenderer
         }
-        return renderer
+        return MKOverlayRenderer()
     }
-    
     func animateToLocation(location: CLLocationCoordinate2D) {
         let region = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: defaultDelta, longitudeDelta: defaultDelta))
         DispatchQueue.main.async {
             self.mapView.setRegion(region, animated: true)
         }
     }
-    
 }
-
